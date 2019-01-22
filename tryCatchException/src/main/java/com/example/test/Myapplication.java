dae470eb0654c77126cded1e02f6b8ac56ea82b4
125073710 <125073710@qq.com>
2019-01-22 18:48:42 +0800

    第一次提交

---
+++ tryCatchException/src/main/java/com/example/test/Myapplication.java
@@ -0,0 +1,23 @@
+package com.example.test;
+
+import android.app.Application;
+
+public class Myapplication extends Application {
+
+    private static Myapplication sInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance = this;
+
+        //在这里为应用设置异常处理程序，然后我们的程序才能捕获未处理的异常
+        CrashHandler crashHandler = CrashHandler.getInstance();
+        crashHandler.init(this);
+    }
+
+    public static Myapplication getInstance() {
+        return sInstance;
+    }
+
+}
